1a1054c44e393455082b4a9b33d86bd8e918222e
Saiham Islam Abid <33659593+SI-Abid@users.noreply.github.com>
2022-11-12 04:39:47 +0000

    lab task 1

---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+echo "Hello world"
+# take two input
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+# add two numbers
+sum=$((a+b))
+# print the sum
+echo "The sum is $sum"
