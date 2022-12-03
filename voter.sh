b15ae08ab6e8f1d32ec258debfec242ee189b798
Saiham Islam Abid <33659593+SI-Abid@users.noreply.github.com>
2022-12-03 03:51:48 +0000

    updated

--- voter.sh
+++ voter.sh
@@ -3,10 +3,8 @@
 read -p "Enter country name: " country
 read -p "Enter age: " age
 
-if [ $country == "BD" ]
-then
-	if [ $age -ge 18 ]
-	then
+if [ $country == "BD" ]; then
+	if [ $age -ge 18 ]; then
 		echo "You can vote"
 	else
 		echo "You can't vote"
