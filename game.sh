f20b1e660933387b05f5fda3c8b10de22a87e25d
SI-Abid <siaabid5323@gmail.com>
2022-12-10 22:09:25 +0600

    game

---
+++ game.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+# Description: Guess the number game
+read -p "Enter your name: " name
+num=$(((RANDOM % 100) + 1))
+counter=0
+prompt="Guess the number: "
+while true; do
+    read -p "$prompt" guess
+    if [[ $guess -eq $num ]]; then
+        echo "You guessed it!"
+        break
+    elif [[ $guess -gt $num ]]; then
+        echo "Too high!"
+    else
+        echo "Too low!"
+    fi
+    ((counter++))
+done
+echo "You guessed it in $counter tries."
+# Write the name and number of tries to a file
+echo "$name $counter" >> count.txt
